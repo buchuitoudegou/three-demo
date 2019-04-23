527b6b364cde5a29e7e13693c2ea6a8b9a8eb947
buchuitoudegou <756541536@qq.com>
2019-04-23 23:30:21 +0800

    change camera

--- src/index.js
+++ src/index.js
@@ -4,23 +4,26 @@ const THREE = require('three');
 
 window.onload = function() {
   var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+  // var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
   var renderer = new THREE.WebGLRenderer();
-  renderer.setSize(document.body.offsetWidth, document.body.offsetHeight);
+  renderer.setSize(600, 600 * 0.75);
+  renderer.setClearColor(0xf0f0f0)
   document.body.appendChild( renderer.domElement );
-
-  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  var cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
-
-  camera.position.z = 5;
-
-  setInterval(() => {
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
-  }, 50);
+  var cube = new THREE.Mesh(new THREE.CubeGeometry(1, 1, 1), 
+    new THREE.MeshBasicMaterial({ 
+        color: 0xff0000, 
+        wireframe: true 
+    })
+  ); 
+  scene.add(cube);
+ 
+  // var camera = new THREE.OrthographicCamera(-1, 3, 1.5, -1.5, 1, 10);
+  var camera = new THREE.PerspectiveCamera(45, 400 / 300, 1, 10); 
+  camera.position.set(0, 0, 5);
+  camera.lookAt(0, 0, 0)
+  scene.add(camera); 
+  
   var render = function () {
     requestAnimationFrame( render );
     renderer.render(scene, camera);
